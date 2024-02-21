d987464d1ebf6b95ac51db3b6dbe7dec6255806a
KiranAI007 <kiranrkrishnan07@gmail.com>
2024-02-21 20:34:18 +0530

    second commit

--- webScrapper/app.py
+++ webScrapper/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen
+from urllib.request import urlopen as uReq
 import logging
 
 app = Flask(__name__)
@@ -10,6 +10,19 @@ app = Flask(__name__)
 @app.route("/", methods = ['GET'])
 def homepage():
     return render_template("index.html")
+
+@app.route("/review", methods = ['GET','POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            searchString = request.form['content']
+            flipKart_url = "https://www.flipkart.com/search?q=" + searchString
+            uClient = uReq(flipKart_url)
+            flipKartPage = uClient.read()
+            uClient.close()
+            flipkart_html = bs(flipKartPage,"html.parser")
+        except Exception as e:
+            print(e)
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0')
